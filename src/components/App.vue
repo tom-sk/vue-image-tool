4793da1a19979a41e23f44dc7589553a33be3c62
Tom <thomasscullyking@hotmail.com>
2017-10-04 07:17:46 +0100

    added more detail edit controls

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,13 +2,36 @@
   <div id="app">
 
     <div class='left' :style="borderObject">
-      <div class='overlay' :style="[textObject, overlayObject]">
-        <div class='quote'>Quote</div>
+      <div class='overlay' :style="overlayObject">
+
+        <div class="quote">
+          <div class='' :style="quoteTextObject">The Way Get Started Is To Quit Talking And Begin Doing</div>
+
+          
+
+          <div class='author' :style="authorTextObject">
+            <div class="style-bar" :style='barObject'></div>
+            Walt Disney
+          </div>
+
+        </div>
+
       </div>
     </div>
 
     <div class='right'>
-       
+<!-- IMAGE BORDER -->
+<p>Image Border</p>
+      <button @click='imageBorderChange' value='display'>
+        Border
+      </button>
+
+      <button @click='imageBorderChange' value='none'>
+        None
+      </button> 
+<br >
+<!-- COLOUR CHANGE -->
+<p>Color</p>
         <vue-slider ref="slider" v-model="rValue" tooltip='false' :min='0' :max='255' :processStyle='rSliderColor'></vue-slider>
         <input v-model="rValue" type='number'>
 
@@ -19,29 +42,51 @@
         <input v-model="bValue" type='number'>
 
           <br/>
-
-      <button 
-        @click='textPositionChange' 
-        value='flex-start'>
+<!-- QUOTE TEXT CHANGE -->
+<p>Quote</p>
+      <button @click='quoteTextPositionChange' value='left'>
         Left
       </button>
 
-      <button  
-        @click='textPositionChange' 
-        value='center'>
+      <button @click='quoteTextPositionChange' value='center'>
         Center
       </button> 
 
-      <button  
-        @click='textPositionChange' 
-        value='flex-end'>
+      <button @click='quoteTextPositionChange' value='right'>
         Right
       </button>
 
-      <vue-slider ref="slider" v-model="textStyle.fontSize" tooltip='false' :min='10' :max='40' :processStyle='processStyle2'></vue-slider>
-      <input v-model="textStyle.fontSize" type='number'>
+      <!-- font size/type  -->
+      <vue-slider ref="slider" v-model="quoteTextStyle.fontSize" tooltip='false' :min='6' :max='40' :processStyle='processStyle2'></vue-slider>
+      <input v-model="quoteTextStyle.fontSize" type='number'>
 
-      <select v-model='textStyle.fontType'>
+      <select v-model='quoteTextStyle.fontType'>
+          <option value="Arial">Arial</option>
+          <option value="Courier">Courier</option>
+          <option value="Verdana">Verdana</option>
+          <option value="Georgia">Georgia</option>
+        </select>
+
+      <br />
+<!-- AUTHOR TEXT CHANGE -->
+<p>Author</p>
+      <button @click='authorTextPositionChange' value='left'>
+        Left
+      </button>
+
+      <button @click='authorTextPositionChange' value='center'>
+        Center
+      </button> 
+
+      <button @click='authorTextPositionChange' value='right'>
+        Right
+      </button>
+
+      <!-- font size/type  -->
+      <vue-slider ref="slider" v-model="authorTextStyle.fontSize" tooltip='false' :min='6' :max='40' :processStyle='processStyle2'></vue-slider>
+      <input v-model="authorTextStyle.fontSize" type='number'>
+
+      <select v-model='authorTextStyle.fontType'>
           <option value="Arial">Arial</option>
           <option value="Courier">Courier</option>
           <option value="Verdana">Verdana</option>
@@ -50,28 +95,46 @@
 
       <br />
 
-
+<!-- STYLE BAR -->
+<p>Style Bar</p>
       <button 
-        @click='overlayChange' 
-        value='overlay'>
+        @click='barPositionChange' 
+        value='left'>
+        Left
+      </button>
+
+      <button @click='barPositionChange' value='center'>
+        Center
+      </button> 
+
+      <button @click='barPositionChange' value='right'>
+        Right
+      </button>
+
+    <br >
+       <button @click='barShow' value='display'>
+         Seperator 
+      </button> 
+
+      <button @click='barShow' value='none'>
+        None
+      </button>
+    <br />
+<!-- OVERLAY CHANGE -->
+<p>Overlay</p>
+      <button @click='overlayChange' value='overlay'>
         Overlay
       </button>
 
-      <button  
-        @click='overlayChange' 
-        value='border'>
+      <button @click='overlayChange' value='border'>
         Border
       </button> 
 
-      <button  
-        @click='overlayChange' 
-        value='bars'>
+      <button @click='overlayChange' value='bars'>
         Bars
       </button>
 
-      <button  
-        @click='overlayChange' 
-        value='none'>
+      <button @click='overlayChange' value='none'>
         None
       </button>
 
@@ -93,7 +156,12 @@ export default {
       rValue:1,
       gValue:1,
       bValue:1,
-      textStyle: {
+      quoteTextStyle: {
+        position: 'center',
+        fontSize: '25',
+        fontType: 'arial'
+      },
+      authorTextStyle: {
         position: 'center',
         fontSize: '25',
         fontType: 'arial'
@@ -105,6 +173,15 @@ export default {
         borderLeft : '',
         borderRight: ''
       },
+      barStyle: {
+        marginLeft: '',
+        marginRight: '',
+        color: '',
+        borderWidth: ''
+      },
+      borderStyle: {
+        border: ''
+      },
       rSliderColor: {"backgroundColor": `#F44336`},
       gSliderColor: {"backgroundColor": `#00C853`},
       bSliderColor: {"backgroundColor": `#03A9F4`},
@@ -114,7 +191,8 @@ export default {
     borderObject(){
       return {
         'border-color': `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`,
-        'color' : `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`
+        'color' : `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`,
+        'border-width': this.borderStyle.border
       }
     },
     processStyle2(){
@@ -122,12 +200,20 @@ export default {
         "backgroundColor": `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`
       }
     },
-    textObject(){
+    quoteTextObject(){
       return {
-        'font-size':this.textStyle.fontSize + 'px',
-        'font-family': this.textStyle.fontType,
+        'font-size':this.quoteTextStyle.fontSize + 'px',
+        'font-family': this.quoteTextStyle.fontType,
         'align-items': 'center',
-        'justify-content':this.textStyle.position
+        'text-align':this.quoteTextStyle.position
+      }
+    },
+    authorTextObject(){
+      return {
+        'font-size':this.authorTextStyle.fontSize + 'px',
+        'font-family': this.authorTextStyle.fontType,
+        'align-items': 'center',
+        'text-align':this.authorTextStyle.position
       }
     },
     overlayObject(){
@@ -140,14 +226,25 @@ export default {
        'border-color': `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`
       //  'border': this.overlayStyle.border
       }
+    },
+    barObject(position){
+      return {
+        'margin-right': this.barStyle.marginRight,
+        'margin-left': this.barStyle.marginLeft,
+        'border-color': `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`,
+        'border-width' : this.barStyle.borderWidth
+      }
     }
   },
   methods: {
-    textPositionChange(e){
-      this.textStyle.position = e.target.value;
+    quoteTextPositionChange(e){
+      this.quoteTextStyle.position = e.target.value;
+    },
+    authorTextPositionChange(e){
+      this.authorTextStyle.position = e.target.value;
     },
     fontChange(e){
-      this.textStyle.fontType = e.target.value;
+      this.quoteTextStyle.fontType = e.target.value;
     },
     overlayChange(e){
       var borderColor = `rgb(${this.rValue}, ${this.gValue}, ${this.bValue})`;
@@ -186,39 +283,39 @@ export default {
         this.overlayStyle.borderRight = noBorder;
         this.overlayStyle.borderBottom = noBorder;
       }
+    },
+    barPositionChange(e){
+      if(e.target.value == 'right'){
+        // this.barStyle.margin = 'auto';
+        this.barStyle.marginRight = '0px';
+        this.barStyle.marginLeft = 'auto';
+      } else if(e.target.value == 'left'){
+        this.barStyle.marginLeft = '0px';
+        this.barStyle.marginRight = 'auto';
+
+      } else if(e.target.value == 'center') {
+        
+        this.barStyle.marginRight = 'auto';
+        this.barStyle.marginLeft = 'auto';
+
+      }
+    },
+    barShow(e){
+      if(e.target.value == 'none'){
+        this.barStyle.borderWidth = '0px'
+      } else if(e.target.value == 'display'){
+        this.barStyle.borderWidth = '2px'
+      }
+    },
+    imageBorderChange(e){
+      if(e.target.value == 'none'){
+        this.borderStyle.border = '0px'
+      } else if(e.target.value == 'display'){
+        this.borderStyle.border = '2px'
+      }
     }
   }
 }
 </script>
 
-<style scoped>
-#app {
-  width: 500px;
-  height: 200px;
-  display:flex;
-}
-.left, .right {
-  width:50%;
-  position: relative;
-}
-
-.left {
-  border: 2px solid black;
-  display:flex;
-  align-items: center;
-  justify-content: center;
-  
-}
-
-.quote{
-  z-index:10;
-  padding:20px;
-}
-.overlay {
-  z-index: 1;
-  display:flex;
-  width: 87%;
-  height:87%;
-}
-
-</style>
+<style src="/Users/thomasscully-king/Desktop/Desktop/web/Vue/vue-image-tool/src/assests/style.css">
